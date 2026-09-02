448f2ee8bec8605a26342239a457161dd3f9b643
ahmfaadli <13ahmadfadli13@gmail.com>
2026-09-03 00:04:49 +0700

    Fix Vercel case sensitive imports

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,16 @@ import Tools from "./components/Tools";
 import Footer from "./components/Footer";
 
 // Pages
-import IoT from "./pages/Skills/IoT";
-import LaptopStore from "./pages/Skills/LaptopStore";
-import Covidid from "./pages/Skills/Covidid";
-import Moveapp from "./pages/Skills/Moveapp";
-import Sembako from "./pages/Skills/Sembako";
-import Nusaloka from "./pages/Skills/Nusaloka";
-import Restauran from "./pages/Skills/Restauran";
-import Projects from "./pages/Project/Projects";
-import CertificatesPage from "./pages/Certificate/Certificates";
+import IoT from "./Pages/Skills/IoT";
+import LaptopStore from "./Pages/Skills/LaptopStore";
+import Covidid from "./Pages/Skills/Covidid";
+import Moveapp from "./Pages/Skills/Moveapp";
+import Sembako from "./Pages/Skills/Sembako";
+import Nusaloka from "./Pages/Skills/Nusaloka";
+import Restauran from "./Pages/Skills/Restauran";
+
+import Projects from "./Pages/Project/Projects";
+import CertificatesPage from "./Pages/Certificate/Certificates";
 
 function App() {
   const [introDone, setIntroDone] = useState(false);
@@ -52,22 +53,15 @@ function App() {
 
       {/* Skills */}
       <Route path="/skills/IoT" element={<IoT />} />
-
       <Route path="/skills/LaptopStore" element={<LaptopStore />} />
-
       <Route path="/skills/Covidid" element={<Covidid />} />
-
       <Route path="/skills/Moveapp" element={<Moveapp />} />
-
       <Route path="/skills/Sembako" element={<Sembako />} />
-
       <Route path="/skills/Nusaloka" element={<Nusaloka />} />
-
       <Route path="/skills/Restauran" element={<Restauran />} />
 
       {/* Projects */}
       <Route path="/projects" element={<Projects />} />
-
       <Route path="/certificates" element={<CertificatesPage />} />
     </Routes>
   );
